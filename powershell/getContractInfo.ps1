87a1991dbd1840f7168ebaad90bb5fa1d60b03e4
Sandeep Kumar <58764439+sandkum5@users.noreply.github.com>
2022-03-01 14:17:55 -0800

    Update getContractInfo.ps1

--- powershell/getContractInfo.ps1
+++ powershell/getContractInfo.ps1
@@ -6,5 +6,16 @@ $ApiParams = @{
 }
 
 Set-IntersightConfiguration @ApiParams
+
+# Get API returned object Count
+$deviceCount = (Get-IntersightAssetDeviceContractInformation -Count $true).Count
+
+# Add 1 to the Quotient
+$loopCount = [System.math]::Floor($deviceCount / 100) + 1
+
 # Get Device Contract Info and write to file: contractInfo.csv 
-Get-IntersightAssetDeviceContractInformation | select ContractStatus,ContractStatusReason,ServiceDescription,ServiceLevel,ServiceStartDate,ServiceEndDate,SalesOrderNumber,PurchaseOrderNumber,PlatformType,DeviceType,DeviceId -ExpandProperty Contract  | select DeviceId,DeviceType,PlatformType,ContractNumber,LineStatus,ContractStatus,ContractStatusReason,ServiceDescription,ServiceLevel,ServiceStartDate,ServiceEndDate,SalesOrderNumber,PurchaseOrderNumber | ConvertTo-Csv | Out-File "./contractInfo.csv"
+for ($i = 0; $i -le $loopCount; $i++) {
+    $top = 100
+    $skip = ($i * 100)
+    Get-IntersightAssetDeviceContractInformation -Top $top -Skip $skip | Select -ExpandProperty Results | select ContractStatus,ContractStatusReason,ServiceDescription,ServiceLevel,ServiceStartDate,ServiceEndDate,SalesOrderNumber,PurchaseOrderNumber,PlatformType,DeviceType,DeviceId -ExpandProperty Contract  | select DeviceId,DeviceType,PlatformType,ContractNumber,LineStatus,ContractStatus,ContractStatusReason,ServiceDescription,ServiceLevel,ServiceStartDate,ServiceEndDate,SalesOrderNumber,PurchaseOrderNumber  | ConvertTo-Csv | Out-File "./contractInfo.csv"
+}
