64652243448a9a4446c361119628f8ccd946ad16
Sandeep Kumar <58764439+sandkum5@users.noreply.github.com>
2022-03-14 11:15:12 -0700

    Update harFileParser.py

--- harFileParser.py
+++ harFileParser.py
@@ -13,15 +13,13 @@ filename = input("Please enter the HAR Filename: ")
 with open(filename, "r") as f:
     har_data = f.read()
     json_data = json.loads(har_data)
-    for i in range(len(json_data["log"]["entries"])):
-        entry = json_data["log"]["entries"][i]
+    for entry in json_data["log"]["entries"]:
         http_method = entry["request"]["method"]
         http_url = entry["request"]["url"]
         for cookie in entry["request"]["cookies"]:
             if cookie["name"] == "X-Starship-Token":
                 x_startship_token = cookie["value"]
-        http_response_data = entry["response"]["content"]
-        # print(f"{http_method} : {http_url} : {http_response_data}")
+        # http_response_data = entry["response"]["content"]["text"]
         print("")
         print(f"HTTP METHOD       : {http_method}")
         print(f"X-Startship-Token : {x_startship_token}")
