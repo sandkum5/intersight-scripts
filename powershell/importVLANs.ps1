87539bd757af2636f699e838899ad2b60f9f538b
Sandeep Kumar <58764439+sandkum5@users.noreply.github.com>
2022-10-15 00:17:16 -0700

    Create importVLANs.ps1

---
+++ powershell/importVLANs.ps1
@@ -0,0 +1,43 @@
+# Script to import VLAN's in Intersight
+# Takes a CSV file exported from UCSM
+$ApiParams = @{
+    BasePath          = "https://intersight.com"
+    ApiKeyId          = Get-Content -Path "./ApiKey.txt" -Raw
+    ApiKeyFilePath    = $pwd.Path + "/SecretKey.txt"
+    HttpSigningHeader = @("(request-target)", "Host", "Date", "Digest")
+}
+
+Set-IntersightConfiguration @ApiParams
+
+#Get vlans from csv
+$vlanList = Import-Csv ./vlans.csv -Header @("Name", "ID")
+
+
+# Uncomment below two lines if the VLAN/Multicast Policies already exist
+# $vlanPolicy = Get-IntersightFabricEthNetworkPolicy -Name "Cigna-VLAN-Policy" | Get-IntersightMoMoRef
+# $mcastPolicy = Get-IntersightFabricMulticastPolicy -Name "MCAST" | Get-IntersightMoMoRef
+
+
+# Comment lines 20,22-23,25-26 if VLAN/Multicast Policies are already created
+$orgObj = Get-IntersightOrganizationOrganization -Name "default"
+
+Write-Host "Create VLAN Policy" -ForegroundColor Green
+$ethNetworkPolicy = New-IntersightFabricEthNetworkPolicy -Name "demo1" -Organization $orgObj
+
+Write-Host "Create Multicast Policy" -ForegroundColor Green
+$multicastPolicy = New-IntersightFabricMulticastPolicy -Name "demo1" -Organization $orgObj -SnoopingState "Enabled" -QuerierState "Disabled"
+
+
+foreach($vlan in $vlanList) {
+    if ($vlan.Name -eq "Name"){ continue }
+    if ($vlan.ID -eq 1) { continue }
+
+    $vlanId = $vlan.ID
+    # $vlanName = $vlan.Name
+    $vlanName = "VLAN" + $vlanId.ToString()
+
+    # Add VLAN to the VLAN Policy
+    New-IntersightFabricVlan -AutoAllowOnUplinks $false -Name $vlanName -VlanId $vlanId -MulticastPolicy $multicastPolicy -EthNetworkPolicy $ethNetworkPolicy | Out-Null
+
+    Write-Host "Created VLAN $vlanName .." -ForegroundColor Green
+}
