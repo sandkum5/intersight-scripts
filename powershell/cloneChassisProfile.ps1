b3641f24389408d2321c2280caf79a61d4327aee
Sandeep Kumar <58764439+sandkum5@users.noreply.github.com>
2022-11-16 19:25:34 -0800

    Create cloneChassisProfile.ps1

---
+++ powershell/cloneChassisProfile.ps1
@@ -0,0 +1,61 @@
+<#
+    Clone Chassis Profile
+    Note:
+        Chassis Profile clone only works when the current and target Organization are same.
+#>
+
+Function Invoke-CloneChassisProfile {
+    Param (
+        # Default Values
+        $OrgName = 'default',
+        $NewOrgName = 'default',
+        $ChassisProfileName = 'Chassis-Profile',
+        $NewChassisProfileName = 'clone-profile-1'
+    )
+
+    # Get Orgs
+    $Org = Get-IntersightOrganizationOrganization -Name $OrgName
+    $OrgObj = $org | Get-IntersightMoMoRef
+    $NewOrg = Get-IntersightOrganizationOrganization -Name $NewOrgName
+    $NewOrgObj = $newOrg | Get-IntersightMoMoRef
+
+    # Get Chassis Profile Info
+    $ChassisProfile = Get-IntersightChassisProfile -Name $ChassisProfileName -Organization $OrgObj
+    $ChassisProfileNewOrg = Get-IntersightChassisProfile -Name $NewChassisProfileName -Organization $NewOrgObj
+
+    if ($ChassisProfileNewOrg) {
+        Write-Host "Chassis Profile $NewChassisProfileName, Moid: $($ChassisProfileNewOrg.Moid) already exits under Org: $NewOrg" -ForegroundColor Green
+        
+        return $ChassisProfileNewOrg
+    } else {
+        # Initialize Port Policy Source
+        $ChassisProfileSrc = Initialize-IntersightMoBaseMo -ClassId "ChassisProfile" -ObjectType "ChassisProfile" -Moid $ChassisProfile.Moid
+
+        # Initialize Port Policy Target
+        $ChassisProfileTarget = Initialize-IntersightChassisProfile -Name $NewChassisProfileName -Organization $NewOrgObj
+
+        # Create Port Policy Clone
+        New-IntersightBulkMoCloner -Sources $ChassisProfileSrc -Targets $ChassisProfileTarget -Organization $NewOrgObj
+
+        $ChassisProfileNewOrg = Get-IntersightChassisProfile -Name $NewChassisProfileName -Organization $NewOrgObj
+
+        if ($ChassisProfileNewOrg) {
+            Write-Host "Chassis Profile $NewChassisProfileName, Moid: $($ChassisProfileNewOrg.Moid) created under Org: $NewOrgName" -ForegroundColor Yellow
+
+            return $ChassisProfileNewOrg
+        }
+    }
+}
+
+
+$ApiParams = @{
+    BasePath          = "https://intersight.com"
+    ApiKeyId          = Get-Content -Path "./apiKey.txt" -Raw
+    ApiKeyFilePath    = $pwd.Path + "/SecretKey.txt"
+    HttpSigningHeader = @("(request-target)", "Host", "Date", "Digest")
+}
+
+Set-IntersightConfiguration @ApiParams
+
+# Run the Function
+Invoke-CloneChassisProfile -OrgName 'default' -NewOrgName 'default' -ChassisProfileName 'Chassis-Profile' -NewChassisProfileName 'clone-lab-profile-2'
